48fcc67c0acfc962efeb757de9179209d2197882
radshodam <mohamadzareai@gmail.com>
2023-02-20 11:22:49 +0330

    add night and day and vibrate button

--- weather/src/pages/index.tsx
+++ weather/src/pages/index.tsx
@@ -37,44 +37,71 @@ const Weather: React.FC = () => {
   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const newCity = e.currentTarget.city.value;
+    navigator.vibrate([100, 50, 100]);
     setCity(newCity);
   };
 
   let backgroundClass = "";
   if (weatherData) {
     const condition = weatherData.weather[0].main.toLowerCase();
+    const description = weatherData.weather[0].description.toLowerCase();
+    const sunrise = new Date(weatherData.sys.sunrise * 1000);
+    const sunset = new Date(weatherData.sys.sunset * 1000);
+    const now = new Date();
+
+    if (now > sunset || now < sunrise) {
+      // night time
+      backgroundClass = "night";
+    } else {
+      // day time
+      backgroundClass = "day";
+    }
+
     switch (condition) {
       case "thunderstorm":
-        backgroundClass = "thunderstorm";
+        backgroundClass += " thunderstorm";
         break;
       case "drizzle":
-        backgroundClass = "drizzle";
+        backgroundClass += " drizzle";
         break;
       case "rain":
-        backgroundClass = "rain";
+        backgroundClass += " rain";
         break;
       case "snow":
-        backgroundClass = "snow";
+        backgroundClass += " snow";
         break;
       case "clear":
-        backgroundClass = "clear sunset";
+        backgroundClass += " clear";
         break;
       case "clouds":
-        backgroundClass = "clouds";
+        if (description.includes("few") || description.includes("scattered")) {
+          backgroundClass += " clouds-sun";
+        } else if (
+          description.includes("broken") ||
+          description.includes("overcast")
+        ) {
+          backgroundClass += " clouds";
+        }
         break;
       default:
-        backgroundClass = "default";
+        backgroundClass += " default";
         break;
     }
   }
 
   return (
     <div
-      className={` bg-center bg-cover h-screen bg-no-repeat w-screen ${backgroundClass}`}
+      className={` ${backgroundClass} bg-center bg-cover h-screen bg-no-repeat w-screen `}
     >
       <div className="flex justify-center mx-auto w-full items-center h-full">
-        <div className="w-11/12 mx-2 p-10 rounded-md lg:w-1/4 backdrop-blur-sm bg-white/30 ">
-          <h1 className="font-bold text-center py-4 text-gray-800 text-shadow-md text-4xl ">
+        <div
+          className={`w-11/12 mx-2 p-10 rounded-md lg:w-1/4 backdrop-blur-sm  ${
+            backgroundClass.includes("night")
+              ? "bg-black/40 text-white "
+              : "bg-white/40 text-gray-800 "
+          }`}
+        >
+          <h1 className="font-bold text-center py-4 text-shadow-md text-4xl ">
             Weather App
           </h1>
           <div>
@@ -119,17 +146,22 @@ const Weather: React.FC = () => {
             onSubmit={handleSearch}
             className="flex justify-center flex-col"
           >
+            {/* bg-gradient-to-r from-cyan-500 to-blue-500 */}
             <div className="mt-1">
               <input
                 type="text"
                 name="city"
                 id="name"
-                className="block w-full py-2 rounded-md border-gray-300 px-4 shadow-sm focus:border-sky-300 focus:ring-sky-300 sm:text-sm"
+                className={`block  w-full py-2 rounded-md border-gray-300 px-4 shadow-sm sm:text-sm"`}
                 placeholder="please Choose City :) "
               />
             </div>
             <button
-              className="inline-flex  backdrop-blur-sm bg-white/60 shadow-lg mx-auto mt-8 w-32 items-center rounded-md  bg-white px-4 py-2 text-base font-medium text-gray-700  hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-sky-300 focus:ring-offset-2"
+              className={`inline-flex  backdrop-blur-sm ${
+                backgroundClass.includes("night")
+                  ? "bg-black/40 text-gray-200  hover:bg-gray-700 "
+                  : "bg-white/80 text-gray-800 hover:bg-gray-50 "
+              } shadow-lg mx-auto mt-8 w-32 items-center rounded-md px-4 py-2 text-base font-medium  focus:outline-none focus:ring-2 focus:ring-sky-300 focus:ring-offset-2`}
               type="submit"
             >
               search City!
